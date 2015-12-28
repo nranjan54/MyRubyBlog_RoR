0a4026ccad293d1536bd21d982de30b59537a677
nranjan54 <ranjan.k.niraj@gmail.com>
2015-12-28 15:28:59 +0530

    Heroku update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+	def index
+		@content_first = 'This is a smaple text for my Ruby Blog';
+		@content_second = 'this is second post';
+	end
+
+	def new
+		
+	end
+
+	def create
+
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		
+	end
+
 end
