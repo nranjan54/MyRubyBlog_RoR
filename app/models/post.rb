4f73514615a62e945509145d3133275ad79f33db
nranjan54 <ranjan.k.niraj@gmail.com>
2015-12-30 01:16:29 +0530

    Made the Post Controller, Model and Views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+	#attr_accessible :title, :body, :category_id, :author_id
+	belongs_to :category
+	def create
+		Post.create(post_params)
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body, :category_id, :author_id)
+	end
 end
