71f8f1e0c572d060475b317cd975476549731428
nranjan54 <ranjan.k.niraj@gmail.com>
2015-12-30 01:16:29 +0530

    Made the Post Controller, Model and Views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,17 @@
+#class Post
+#	has_many :posts
+#end
+
 class Category < ActiveRecord::Base
+	#attr_accessible :name
+	has_many :posts
+	def create
+		Category.create(category_params)
+	end
+
+	private
+
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
